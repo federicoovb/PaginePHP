3566d1acce1257f20b36e7253109ed63d9f8859a
federicoovb <violaboros.federico@studenti.salesianisesto.it>
2021-09-30 19:50:18 +0200

    upload all

---
+++ Pagina1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+    <form action="Pagina2.php" methods = "GET">        
+        <fieldset>
+            <legend>
+                Login:
+            </legend>
+            <p>nome: <input type="text" name="varnome"></p>
+            <p>cognome: <input type="text" name="varcognome"></p>
+            <input type="submit" value="spedisci">
+        </fieldset>
+    </form>
+</body>
+</html>
